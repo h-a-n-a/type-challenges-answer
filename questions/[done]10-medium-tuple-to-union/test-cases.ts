42c8be045297e5801184fb2e882db17145f368c4
H <andywangsy@gmail.com>
2020-10-29 05:01:06 +0000

    feat: add answer 10

--- questions/[done]10-medium-tuple-to-union/test-cases.ts
+++ questions/[done]10-medium-tuple-to-union/test-cases.ts
@@ -4,3 +4,6 @@ type cases = [
   Expect<Equal<TupleToUnion<[123, '456', true]>, 123 | '456' | true>>,
   Expect<Equal<TupleToUnion<[123]>, 123>>,
 ]
+
+// answer
+type TupleToUnion<T extends any[]> = T[number]
