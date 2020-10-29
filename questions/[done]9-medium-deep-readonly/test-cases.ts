15881f9328e86d691af9367766bbab25b280654e
H <andywangsy@gmail.com>
2020-10-29 05:15:11 +0000

    feat: add answer 9

--- questions/[done]9-medium-deep-readonly/test-cases.ts
+++ questions/[done]9-medium-deep-readonly/test-cases.ts
@@ -37,3 +37,11 @@ type Expected = {
     }
   }
 }
+
+// answer
+
+type DeepReadonly<T> = T extends Function ? T 
+  : T extends object ? {
+    readonly [K in keyof T]: DeepReadonly<T[K]>
+  } 
+  : T
