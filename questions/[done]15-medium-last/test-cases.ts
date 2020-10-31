f0f839199f2d9b0977ba3e9cd1e338b344ff5197
H <andywangsy@gmail.com>
2020-10-31 04:58:09 +0000

    feat: add answer 15

--- questions/[done]15-medium-last/test-cases.ts
+++ questions/[done]15-medium-last/test-cases.ts
@@ -4,3 +4,7 @@ type cases = [
   Expect<Equal<Last<[3, 2, 1]>, 1>>,
   Expect<Equal<Last<[() => 123, { a: string }]>, { a: string }>>,
 ]
+
+// answer
+
+type Last<T extends any[]> = T extends [...infer _, infer U] ? U : never
