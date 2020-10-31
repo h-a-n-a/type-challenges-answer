33ac468546529f5810345ed67b22158b767603ce
H <andywangsy@gmail.com>
2020-10-31 04:59:19 +0000

    feat: add answer 16

--- questions/[done]16-medium-pop/test-cases.ts
+++ questions/[done]16-medium-pop/test-cases.ts
@@ -4,3 +4,7 @@ type cases = [
   Expect<Equal<Pop<[3, 2, 1]>, [3, 2]>>,
   Expect<Equal<Pop<['a', 'b', 'c', 'd', ]>, ['a', 'b', 'c']>>,
 ]
+
+// answer
+
+type Pop<T extends any[]> = T extends [...infer U, infer _] ? U : never;
