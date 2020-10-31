8e0b54fa187fad5fc38c9f277aeeb265f72c0d4d
H <andywangsy@gmail.com>
2020-10-31 04:54:38 +0000

    feat: add answer 11

--- questions/[done]11-easy-tuple-to-object/test-cases.ts
+++ questions/[done]11-easy-tuple-to-object/test-cases.ts
@@ -5,3 +5,9 @@ const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
 type cases = [
   Expect<Equal<TupleToObject<typeof tuple>, { tesla: 'tesla'; 'model 3': 'model 3'; 'model X': 'model X'; 'model Y': 'model Y'}>>,
 ]
+
+// answer 
+
+type TupleToObject<T extends readonly any[]> = {
+  [K in T[number]]: K
+}
