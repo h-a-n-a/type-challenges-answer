65c7f29467936d8ba5003d42b066814a7866178c
h-a-n-a <andywangsy@gmail.com>
2021-01-09 18:31:18 +0800

    feat: add answer 459

--- questions/[done]459-medium-flatten/test-cases.ts
+++ questions/[done]459-medium-flatten/test-cases.ts
@@ -7,3 +7,7 @@ type cases = [
   Expect<Equal<Flatten<[1, 2, [3, 4], [[5]]]>, [1, 2, 3, 4, 5]>>,
   Expect<Equal<Flatten<[{foo: 'bar'; 2: 10}, 'foobar']>, [{foo: 'bar'; 2: 10}, 'foobar']>>,
 ]
+
+// answer
+
+type Flatten<T extends Array<any>> = T extends [infer P, ...infer U] ? P extends Array<any> ? [...Flatten<P>, ...Flatten<U>]: [P, ...Flatten<U>] : []
