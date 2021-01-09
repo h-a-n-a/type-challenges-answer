a56c809287108b77c0ccae96b0b7360dc0d8d9f9
h-a-n-a <andywangsy@gmail.com>
2021-01-09 18:57:00 +0800

    feat: add answer 57/59

--- questions/[done]57-hard-get-required/test-cases.ts
+++ questions/[done]57-hard-get-required/test-cases.ts
@@ -4,3 +4,13 @@ type cases = [
     Expect<Equal<GetRequired<{ foo: number, bar?: string }>, { foo: number }>>,
     Expect<Equal<GetRequired<{ foo: undefined, bar?: undefined }>, { foo: undefined }>>,
 ]
+
+// answer
+
+type GetRequiredKeys<T extends object> = {
+    [K in keyof T]: Pick<T, K> extends {
+        [Key in K]-?: T[Key]
+    } ? K : never;
+}[keyof T]
+
+type GetRequired<T extends object> = Pick<T, GetRequiredKeys<T>>
--- questions/[done]59-hard-get-optional/test-cases.ts
+++ questions/[done]59-hard-get-optional/test-cases.ts
@@ -4,3 +4,11 @@ type cases = [
     Expect<Equal<GetOptional<{ foo: number, bar?: string }>, { bar?: string }>>,
     Expect<Equal<GetOptional<{ foo: undefined, bar?: undefined }>, { bar?: undefined }>>,
 ]
+
+// answer
+
+type GetOptionalKeys<T extends object> = {
+    [K in keyof T]:  { [Key in K]?: T[K] } extends Pick<T, K> ? K : never
+}[keyof T]
+
+type GetOptional<T extends object> = Pick<T, GetOptionalKeys<T>>
