8cf8ec4f795afa4f60836e6018bedf9c29468dc7
H <andywangsy@gmail.com>
2021-01-19 04:06:50 +0000

    feat: add answer 110

--- questions/110-medium-capitalize/test-cases.ts
+++
@@ -1,8 +0,0 @@
-import { Equal, Expect } from '@type-challenges/utils'
-
-type cases = [
-  Expect<Equal<Capitalize<'foobar'>, 'Foobar'>>,
-  Expect<Equal<Capitalize<'FOOBAR'>, 'FOOBAR'>>,
-  Expect<Equal<Capitalize<'foo bar'>, 'Foo bar'>>,
-  Expect<Equal<Capitalize<''>, ''>>,
-]
---
+++ questions/[done]110-medium-capitalize/test-cases.ts
@@ -0,0 +1,47 @@
+import { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Capitalize<'foobar'>, 'Foobar'>>,
+  Expect<Equal<Capitalize<'FOOBAR'>, 'FOOBAR'>>,
+  Expect<Equal<Capitalize<'foo bar'>, 'Foo bar'>>,
+  Expect<Equal<Capitalize<''>, ''>>,
+]
+
+
+// answer
+
+type Map = {
+  a: "A",
+  b: "B",
+  c: "C",
+  d: "D",
+  e: "E",
+  f: "F",
+  g: "G",
+  h: "H",
+  i: "I",
+  j: "J",
+  k: "K",
+  l: "L",
+  m: "M",
+  n: "N",
+  o: "O",
+  p: "P",
+  q: "Q",
+  r: "R",
+  s: "S",
+  t: "T",
+  u: "U",
+  v: "V",
+  w: "W",
+  x: "X",
+  y: "Y",
+  z: "Z"
+}
+
+type Capitalize<T> = 
+  T extends `${infer U}${infer P}` ? 
+    U extends keyof Map ? 
+      `${Map[U]}${P}` : 
+    `${U}${P}` : 
+  T;
