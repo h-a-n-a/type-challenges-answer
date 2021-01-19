8a690b6f21c07b0ab421ad84f60bd83f27af0201
H <andywangsy@gmail.com>
2021-01-19 03:54:55 +0000

    feat: add answer 106

--- questions/[done]106-medium-trimleft/test-cases.ts
+++ questions/[done]106-medium-trimleft/test-cases.ts
@@ -7,3 +7,12 @@ type cases = [
   Expect<Equal<TrimLeft<'     str     '>, 'str     '>>,
   Expect<Equal<TrimLeft<'   \n\t foo bar '>, 'foo bar '>>,
 ]
+
+// answer
+
+type TrimLeft<T extends string> = 
+  T extends `${infer U}${infer P}` ? 
+    U extends " " | "\n" | "\t" ? 
+      TrimLeft<P> :
+    `${U}${P}` :
+  never;
