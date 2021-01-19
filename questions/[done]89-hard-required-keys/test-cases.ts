fbffa95e9296f2da8a0b98ee971d4fd1944725bb
H <andywangsy@gmail.com>
2021-01-19 03:22:51 +0000

    feat: add answer 89

--- questions/[done]89-hard-required-keys/test-cases.ts
+++ questions/[done]89-hard-required-keys/test-cases.ts
@@ -6,3 +6,11 @@ type cases = [
     Expect<Equal<RequiredKeys<{ a: undefined, b?: undefined, c: string, d: null }>, "a" | "c" | "d">>,
     Expect<Equal<RequiredKeys<{}>, never>>
 ]
+
+
+// answer
+
+type TypeEqual<A, B> = (<T>() => T extends A ? 1 : 2 ) extends (<T>() => T extends B ? 1 : 2) ? true : false
+type RequiredKeys<T extends object> = Exclude<{
+    [K in keyof T]: TypeEqual<Pick<T, K>, Required<Pick<T, K>>> extends true ? K : never
+}[keyof T], undefined>
